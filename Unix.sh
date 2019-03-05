c1ee760bdf26a3d6728dc8bbc805d15345c0cd59
Samuel Lee <lee.yuru.samuel@stee.stengg.com>
2019-03-05 15:43:21 +0800

    Contains only ruby-2.5.3 for Windows x64.

--- Unix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-SRC=$(realpath "$(dirname "$BASH_SOURCE")")
-ruby "${SRC}/nodec/bin/nodec" "${SRC}/Hello_World/HelloWorld.js" -r "${SRC}/Hello_World" -d "${SRC}/tmp" -o "Unix.bin" --clean-tmpdir
--- Windows.bat
+++
@@ -1,2 +0,0 @@
-ruby "nodec/bin/nodec" "Hello_World/HelloWorld.js" -r "Hello_World" -d "tmp" -o "Windows.exe" --clean-tmpdir >> Result.txt 2>&1 
-pause
